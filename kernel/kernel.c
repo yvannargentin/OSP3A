b6f392e0e9d0696d69aef435a8895e16b088c2e4
pannufle <hottinger.jeremy@gmail.com>
2014-11-04 13:17:54 -0800

    1

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -7,5 +7,5 @@ void kernel(void)
 	init_syscalls(); // ecrase interruption 80 pour la notre
         //char *myString = "hello\0";
 	interrupt(0x80,1,"costa rica ticos",0,0);
-	while(1); 	// évite d'aller lire plus loin
+	while(1); // évite d'aller lire plus loin
 }
