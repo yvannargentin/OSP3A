64ff020961d3f9364e351232bb1c85faeacc65be
Ocsav <monstross.ocsav@gmail.com>
2014-11-04 11:43:30 +0100

    Update io.c

--- tools/io.c
+++ tools/io.c
@@ -1,6 +1,6 @@
 extern int interrupt(int number, int ax, int bx, int cx, int dx, int di);
 
-#define addr(idx, off) (x * 256 + y)
+#define addr(idx, off) (idx * 256 + off)
 void print_string(char *buf){
 	int ax = addr(13, 1);
 	int length = sizeof(*buf);
@@ -10,10 +10,11 @@ void print_string(char *buf){
 
 void read_string(char *buf){
 	int i = 0;
+	int ax = addr(0, 0);
 	//Tant que le scan code n'est pas égal à 1C0D
-	while((ax>>8) != 1C0D){	
-		int ax = addr(0, 0);
+	//while((ax>>8) != 1C0D){	
+		ax = addr(0, 0);
 		interrupt(0x16, &ax , 0, 0 , 0, 0); // int 13, ah=0/al=0
 		buf[i++] = (ax<<8)>>8;
-	}
+	//}
 }
