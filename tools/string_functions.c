ae2cf01d0181102de09f730b73de6b3ea2e5477e
Ocsav <monstross.ocsav@gmail.com>
2014-11-04 11:22:26 +0100

    Update string_functions.c

--- tools/string_functions.c
+++ tools/string_functions.c
@@ -7,3 +7,13 @@ void print_string(char *buf){
 	int cx = addr(length, 0);
 	interrupt(0x10, ax , buf, cx , 0, 0); // int 13, ah=13/al=1
 }
+
+void read_string(char *buf){
+	int i = 0;
+	//Tant que le scan code n'est pas égal à 1C0D
+	while((ax>>8) != 1C0D){	
+		int ax = addr(0, 0);
+		interrupt(0x16, &ax , 0, 0 , 0, 0); // int 13, ah=0/al=0
+		buf[i++] = (ax<<8)>>8;
+	}
+}
