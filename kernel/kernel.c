cac8fd094aebd1d6e483d0b952f2ca307b437c26
chwebteam1 <infochwebteam.ch>
2014-11-05 01:12:10 +0100

    print 1 char max fail return read str

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -9,69 +9,14 @@ void read_string(char *buf);
 void kernel(void)
 {
 	char *str;
-	//print_str("entrez un caracteres :");
-	read_string(str);
+	//print_str("entrez un message :");
+	//read_string(&str);
 	//print_str(str);
+	//print_string('a');
+	init_syscalls(); // ecrase interruption 80 pour la notre
+	interrupt(0x80,1,'a',0,0);
+	interrupt(0x80,2,str,0,0);
+	interrupt(0x80,1,str,0,0);
 
-	//init_syscalls(); // ecrase interruption 80 pour la notre
-	//interrupt(0x80,1,str,0,0);
 	while(1); 	// évite d'aller lire plus loin
-}
-
-void print_str(char *buf){ 
-	int i=0; 
-	int ax;
-	int bx = addr(0x0, 0x07);
-	for(i=0;buf[i]!='\0';i++){ 
-		char al = buf[i];
-		ax = addr(0x0E,buf[i]);
-		interrupt(0x10, ax, bx, 0, 0,0);
-	}
-	
-}
-
-void read_string(char *buf){
-	char enter = 0xd; 
-	char endLine = 0xa; 
-	char nullChar = 0x0; 
-	char back = 0x8;
-	int bx = addr(0x0, 0x07);
-	
-	// while loop set up 
-	int i = 0; 
-	char ascii = interrupt(0x16, 0, 0, 0, 0,0);
-	int ax = addr(13, ascii);
-	interrupt(0x10, ax, bx, 0, 0,0);
-
-	// exit key: enter
-	while(ascii!=enter){ 
-
-		// decrements i (up to 0) if a backspace is entered, otherwise increments i
-		// deals with what is in the char array         
-		if(ascii==back&&i>0) {i--;}
-		else if(ascii==back) {i=0;} 
-		else {buf[i]=ascii; i++;}   
-
-		// get next input letter and write it to screen 
-		ascii = interrupt(0x16, 0, 0, 0, 0); 
-		ax = addr(13, ascii);
-		interrupt(0x10, ax, bx, 0, 0,0);
-
-		// clear the display when backspace is clicked 
-		if(ascii==back){
-			ax = addr(13, nullChar);
-			interrupt(0x10, ax, bx, 0, 0,0);
-			
-			ax = addr(13, ascii);
-			interrupt(0x10, ax, bx, 0, 0,0);
-		}
-	}
-
-	// puts end line and null characters at the end of the array 
-	buf[i] = endLine; 
-	buf[i+1] = nullChar; 
-
-	// Writes a new line character to the screen 
-	ax = addr(13, endLine);
-	interrupt(0x10, ax, bx, 0, 0,0); 
 }
--- tools/io.c
+++ tools/io.c
@@ -4,23 +4,21 @@ extern int interrupt(int number, int ax, int bx, int cx, int dx, int di);
 void print_string(char *buf){ 
 	int i=0; 
 	int ax, cx, bx;
-
-	//while(buf[i] != '\0')	{
-		char al = buf;
-		ax = addr(0x0e, al);
-		bx = addr(0x00, 0x07);
-		cx = addr(0, 1); //on affiche 1 caractère
-		interrupt(0x10, ax, bx, cx, 0,0);
-	//}
-	
+	char al = buf;
+	ax = addr(0x0e, al);
+	bx = addr(0x00, 0x07);
+	cx = addr(0, 1); //on affiche 1 caractère
+	interrupt(0x10, ax, bx, cx, 0,0);
 }
 
 //affiche les caractères tapés au clavier
 void read_string(char *buf){
 	char ascii;
-	
+	int i = 0;
 	while(ascii != 0xd){
 		ascii = interrupt(0x16, 0, 0, 0, 0);
 		print_string(ascii);
+		buf[i] = ascii;
+		i++;
 	}
 }
