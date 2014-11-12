d129624e66c60127036c4efb5ef5fe5dd1fbd088
Vasco Marques <monstross.ocsav@gmail.com>
2014-11-12 11:33:25 +0100

    struct.h + struct.c

--- tools/structure.c
+++ tools/structure.c
@@ -1,29 +1,8 @@
 #include <string.h>
 #include <stdio.h>
+#include "structure.h"
 
-typedef struct SuperBlock{
-	char signature[8];
-	int nbSecteurs;
-	int tailleBitmap;
-	int tailleFE; //FE = FileEntry
-	int blockReservedFE;
-	int blockReserverContent;
-} SuperBlock;
-
-typedef struct FileEntries{
-	char name[32];
-	int size;
-	int tabIndexes[111]; //111 = (tailleFE-name-size)/2
-} FileEntries;
-
-typedef struct sfs {
-	SuperBlock sb;
-	char bitmap[1024];
-	FileEntries fe;
-	char fileContent[256][1024];
-} SimpleFileSystem;
-
-int main (int argc, char *argv){
+SimpleFileSystem sfscreate (char *sfsName){
 	SimpleFileSystem sfs;
 	strcpy(sfs.sb.signature, "SFSv0100");
 	sfs.sb.nbSecteurs = 2;
@@ -34,7 +13,12 @@ int main (int argc, char *argv){
 
 	//Remplir bitmap que avec des 0
 	int i;
-	for(i=0; i<sizeof(sfs.bitmap); i++){
+	int tailleBitmap = sfs.sb.tailleBitmap*1024
+	for(i=0; i<tailleBitmap; i++){
 		sfs.bitmap[i] = '0';
 	}
+}
+
+int main(int argc, char **argv){
+//
 }
