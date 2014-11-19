277b87bbaffa2f6892a118438bf2993fbb5dc062
Vasco Marques <monstross.ocsav@gmail.com>
2014-11-19 11:24:29 +0100

    commentaires dans io.c

--- kernel/io.c
+++ kernel/io.c
@@ -6,35 +6,34 @@ void print_char(char c) {
 	int ax, cx, bx;
 	char al = c;
 	ax = pack(0x0e, c);
-	bx = pack(0x00, 0x07);
+	bx = pack(0x00, 0x00); //Page number + foreground color
 	cx = pack(0, 1); //on affiche 1 caractère
 	interrupt(0x10, ax, bx, cx, 0,0);
 }
 
 void print_string(char *buf){ 
 	int i=0; 
-	int ax, cx, bx;
 	char al = buf;
 	
-	while(buf[i] !='\0'){
-		print_char(buf[i]);
+	while(buf[i] !='\0'){//Parcours de la chaine
+		print_char(buf[i]); //Affichage d'un seul caractère
 		i += 1;
 	}
-	print_char(10);
-	print_char(13);
+	print_char(10); //Nouvelle ligne
+	print_char(13); //Retour chariot
 }
 
 //affiche les caractères tapés au clavier
 void read_string(char *buf){
-	char ascii;
-	int i = 0;
-	while(ascii != 0xd){
-		ascii = interrupt(0x16, 0, 0, 0, 0);
-		print_char(ascii);
-		buf[i] = ascii;
+	char ascii; //Variable qui va receptionner l'entrée clavier
+	int i = 0;//Initialisation du début de la chaine "buf"
+	while(ascii != 0xd){ //Boucle tant que la touche "enter" n'est pas pressé
+		ascii = interrupt(0x16, 0, 0, 0, 0); //Appel bloquant attendant un caractère
+		print_char(ascii); //Affichage du caractère tappé
+		buf[i] = ascii; //Ajout du caractère tapé au buffer
 		i++;
 	}
-	buf[i] = '\0';
-	print_char(10);
-	print_char(13);
+	buf[i] = '\0'; //indique la fin de la chaine
+	print_char(10); //Nouvelle ligne
+	print_char(13); //Retour chariot
 }
