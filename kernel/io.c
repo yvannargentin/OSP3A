97ced4411d978b2ddd2317ce0fe364c4cbef97e1
pannufle <hottinger.jeremy@gmail.com>
2014-11-19 11:27:00 +0100

    Repassage derriere Vasco pck c'est un noob

--- kernel/io.c
+++ kernel/io.c
@@ -6,8 +6,8 @@ void print_char(char c) {
 	int ax, cx, bx;
 	char al = c;
 	ax = pack(0x0e, c);
-	bx = pack(0x00, 0x00); //Page number + foreground color
-	cx = pack(0, 1); //on affiche 1 caractère
+	bx = pack(0x00, 0x00); 	//Page number + foreground color
+	cx = pack(0, 1); 	//on affiche 1 caractère
 	interrupt(0x10, ax, bx, cx, 0,0);
 }
 
@@ -15,8 +15,8 @@ void print_string(char *buf){
 	int i=0; 
 	char al = buf;
 	
-	while(buf[i] !='\0'){//Parcours de la chaine
-		print_char(buf[i]); //Affichage d'un seul caractère
+	while(buf[i] !='\0'){		//Parcours de la chaine
+		print_char(buf[i]); 	//Affichage d'un seul caractère
 		i += 1;
 	}
 	print_char(10); //Nouvelle ligne
@@ -25,15 +25,15 @@ void print_string(char *buf){
 
 //affiche les caractères tapés au clavier
 void read_string(char *buf){
-	char ascii; //Variable qui va receptionner l'entrée clavier
-	int i = 0;//Initialisation du début de la chaine "buf"
-	while(ascii != 0xd){ //Boucle tant que la touche "enter" n'est pas pressé
-		ascii = interrupt(0x16, 0, 0, 0, 0); //Appel bloquant attendant un caractère
-		print_char(ascii); //Affichage du caractère tappé
-		buf[i] = ascii; //Ajout du caractère tapé au buffer
+	char ascii; 		//Variable qui va receptionner l'entrée clavier
+	int i = 0;		//Initialisation du début de la chaine "buf"
+	while(ascii != 0xd){ 	//Boucle tant que la touche "enter" n'est pas pressé
+		ascii = interrupt(0x16, 0, 0, 0, 0); 	//Appel bloquant attendant un caractère
+		print_char(ascii); 	//Affichage du caractère tappé
+		buf[i] = ascii; 	//Ajout du caractère tapé au buffer
 		i++;
 	}
-	buf[i] = '\0'; //indique la fin de la chaine
+	buf[i] = '\0'; 	//indique la fin de la chaine
 	print_char(10); //Nouvelle ligne
 	print_char(13); //Retour chariot
 }
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,17 +2,14 @@
 *  kernel.c
 */
 
-#define addr(idx, off) (idx * 256 + off)
-
-
 
 void kernel(void) {
 	char *str;
 	
-	// print_string(str); 	// fonctionne
-	init_syscalls(); 	// ecrase interruption 80 pour la notre
+	init_syscalls(); 		// ecrase interruption 80 pour la notre
 	interrupt(0x80,1,"abcd",0,0);
 	
+	// Lit une string et l'ecrit
 	while(1) {
 		interrupt(0x80,2,str,0,0);
 		interrupt(0x80,1,str,0,0);
