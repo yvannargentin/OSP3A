d7e7987ee9876cbdcc09da82374db158a8148067
pannufle <hottinger.jeremy@gmail.com>
2014-11-19 10:19:32 +0100

    Changement

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -7,12 +7,12 @@
 void kernel(void)
 {
 	char *str = "costa rica !";
-	//print_string(str); // fonctionne
-	init_syscalls(); // ecrase interruption 80 pour la notre
-	interrupt(0x80,1,str,0,0);
+	print_string(str); 	// fonctionne
+	// init_syscalls(); 	// ecrase interruption 80 pour la notre
+	// interrupt(0x80,1,"abcd",0,0);
 	
-	//interrupt(0x80,2,str,0,0);
-	//interrupt(0x80,1,str,0,0);
+	// interrupt(0x80,2,str,0,0);
+	// interrupt(0x80,1,str,0,0);
 
 	while(1); 	// évite d'aller lire plus loin
 }
