9327a75365ff6e5bc804a60eff0dc6e1f85fc7eb
Ana Sofia Domingos <ana-sofia.domingos@eig.ch>
2014-11-26 11:06:33 +0100

    update sfsadd

--- tools/sfsadd.c
+++ tools/sfsadd.c
@@ -15,47 +15,49 @@ void sfsadd(SimpleFileSystem sfs , char filename[32]){
 	bool fileInit = false;
 		
 	// lecture du bitmap
-	for (i=0; i< block; i++){
-		if (sfs.bitmap[i] == 0){ // si block libre
+	for (i=0; i< block; i++){	
+		for (j=0; j< 8; j++){
+			if (sfs.bitmap[i]&(1<<j) == 0){ // si bit block libre
 			
-			if (fp == NULL){
-				printf("impossible d'ouvrir le fichier");
-				break; 
-			}else{
-				sfs.bitmap[i] = 1;
-				// initalisation du file entry avec le nom et la taille	
-				if (fileInit == false){
-					for (j=0;j< 32; j++)
-						sfs.fe[i].name[j] = filename[j];
+				if (fp == NULL){
+					printf("impossible d'ouvrir le fichier");
+					break; 
+				}else{
+					sfs.bitmap[i] &= (1<<j); // bit block utilisé
+					// initalisation du file entry avec le nom et la taille	
+					if (fileInit == false){
+						for (j=0;j< 32; j++)
+							sfs.fe[i].name[j] = filename[j];
 					
-					//printf("%s ", sfs.fe[i].name);
+						//printf("%s ", sfs.fe[i].name);
 					
-					// recuperer taille fichier
-					//sfs.fe[i].size = ftell(fp);
-					sfs.fe[i].size = 142;
-					sfs.fe[i].tabIndexes[i] = i;
-					//printf("%s ", sfs.fe[i].size);
-					fileInit = true;
-				}
+						// recuperer taille fichier
+						//sfs.fe[i].size = ftell(fp);
+						sfs.fe[i].size = 142;
+						sfs.fe[i].tabIndexes[i] = i;
+						//printf("%s ", sfs.fe[i].size);
+						fileInit = true;
+					}
 				
-				//printf(" block %d libre ", i);
-				fgets(contenu, taille, fp); // recupere contenu fichier	
+					//printf(" block %d libre ", i);
+					fgets(contenu, taille, fp); // recupere contenu fichier	
 
-				// ajoute le contenu du fichier par block dans le fileContent correspondant
-				for (j=0; j< taille; j++){
-					sfs.fileContent[i][j] = contenu[j];
-					//printf("%c", sfs.fileContent[i][j]);				
-				}
+					// ajoute le contenu du fichier par block dans le fileContent correspondant
+					for (j=0; j< taille; j++){
+						sfs.fileContent[i][j] = contenu[j];
+						//printf("%c", sfs.fileContent[i][j]);				
+					}
 				
-				if(fgetc(fp) != EOF){
-					// vide le tableau contenant le contenu
-					memset (contenu, 0, taille); 
-					// se positionne à l'endroit ou le bout de contenu s'est arreter
-					fseek(fp, taille,0); 
-				}else
-					break;
+					if(fgetc(fp) != EOF){
+						// vide le tableau contenant le contenu
+						memset (contenu, 0, taille); 
+						// se positionne à l'endroit où le bout de contenu s'est arreter
+						fseek(fp, taille,0); 
+					}else
+						break;
+				}
 			}
-		}
 			
+		}
 	}
 }
