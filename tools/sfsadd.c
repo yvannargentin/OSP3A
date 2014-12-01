3f1ea3ed57d0d454290dca6f8c5e1702061c61e6
Ana Sofia Domingos <ana.domingos@hotmail.fr>
2014-12-01 23:32:18 +0100

    mise a jour de sfsadd

--- tools/sfsadd.c
+++ tools/sfsadd.c
@@ -3,66 +3,68 @@
 #include <stdbool.h>
 #include "structure.h"
 
-void sfsadd(SimpleFileSystem *sfs , char filename[32]){
+#define TAILLE 10
+#define BLOCK 256/8
+#define LENGTH 32
+#define BIT 8
+
+void sfsadd(SimpleFileSystem *sfs , char filename[LENGTH]){
 	int i, j, r, h;
-	int block = 256/8;
 	
 	// lecture du fichier et recuperation du contenu
 	FILE *fp;
-	int taille = 10;
-	char contenu[taille];
+	fp = fopen(filename, "r");
+	char contenu[TAILLE];
 	bool fileInit = false;
-		
+	
 	// lecture du bitmap
-	for (i=0; i< block; i++){	
-		for (j=0; j< 8; j++){
-			
+	for (i=0; i< BLOCK; i++){	
+		for (j=0; j< BIT; j++){
 			if ((sfs->bitmap[i]&(1<<j)) == 0){ // si bit block libre
-				fp = fopen(filename, "r");
+				
 				if (fp == NULL){
 					printf("impossible d'ouvrir le fichier\n");
 					return; 
 				}else{
-					sfs->bitmap[i] = (1<<j); // bit block utilisé
-				
+					int index = (i*BIT)+j;
+					sfs->bitmap[index] = 1; // bit block utilisé
+					
 					// initalisation du file entry avec le nom et la taille	
 					if (fileInit == false){
-						for (h=0;h< 32; h++)
+						for (h=0; h< LENGTH; h++)
 							sfs->fe[i].name[h] = filename[h];
 					
-						
-					
 						// recuperer taille fichier
-						//sfs.fe[i].size = ftell(fp);
-						sfs->fe[i].size = 142;
-						sfs->fe[i].tabIndexes[i] = i;
+						sfs->fe[i].size = FileSize(filename);
+						sfs->fe[i].tabIndexes[index] = index;
 						fileInit = true;
 					}
-					/* printf("%d ", sfs.bitmap[i]);
-					printf("%s ", sfs.fe[i].name);
-					printf("%d ", sfs.fe[i].size);*/
 				
-					//printf(" block %d libre ", i);
-					fgets(contenu, taille, fp); // recupere contenu fichier	
-
+					
+					printf("%s ", sfs->fe[i].name);
+					printf("%d ", sfs->fe[i].size);
+					printf("%d ", sfs->fe[i].tabIndexes[i]);
+					
+					fgets(contenu, TAILLE, fp); // recupere contenu fichier	
 					// ajoute le contenu du fichier par block dans le fileContent correspondant
 						
-					for (r=0; r< taille; r++){
-						int index = (r*8)+j-1;
+					for (r=0; r< TAILLE; r++){
+						int index = (r*BIT)+j;
 						sfs->fileContent[index][r] = contenu[r];
-						//printf("%c", sfs.fileContent[index][r]);				
+						printf("%c", sfs->fileContent[index][r]);				
 					}
 				
 					// tant qu'on arrive pas à la fin du fichier
 					if(fgetc(fp) != EOF){
 						// vide le tableau contenant le contenu
-						memset (contenu, 0, taille); 
+						for (r=0; r< TAILLE; r++)
+							contenu[r] = '\0';
 						// se positionne à l'endroit où le bout de contenu s'est arreter
-						fseek(fp, taille-1,0);
-						//printf("\n"); 
+						fseek(fp, TAILLE, 0);
+						printf("\n"); 
 					}else{
 						
-						//printf("\n");	
+						printf("\n");	
 						fileInit = false;
 						return;
 					}
@@ -71,4 +73,22 @@ void sfsadd(SimpleFileSystem *sfs , char filename[32]){
 			
 		}
 	}
+}
+
+
+int FileSize(char filename[LENGTH]){
+	// ouvre le fichier pour le parcourir
+	FILE *fp;
+	fp = fopen(filename, "r");
+	if (fp == NULL){
+		printf("impossible d'ouvrir le fichier\n");
+		return; 
+	}
+
+	// calcul la taille du fichier
+	int taille = 0;
+	while(fgetc(fp) != EOF)
+		taille++;
+
+	return taille;
 }
