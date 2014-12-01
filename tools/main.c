e05f75461e5add1cddc517c9b85991b422e0e0f9
Ana Sofia Domingos <ana.domingos@hotmail.fr>
2014-12-01 22:39:49 +0100

    sfsadd marche git add ./tools/sfsadd.c

--- tools/main.c
+++ tools/main.c
@@ -4,13 +4,13 @@
 #include "structure.h"
 
 extern SimpleFileSystem sfscreate (char *sfsName);
-extern void sfsadd(SimpleFileSystem sfs , char filename[32]);
+extern void sfsadd(SimpleFileSystem *sfs , char filename[32]);
 extern void sfsls(SimpleFileSystem sfs);
 extern void sfsdel(SimpleFileSystem sfs, char filename[32]);
 
 int main(int argc, char **argv){
 	SimpleFileSystem sf = sfscreate("sfs");
-	sfsadd(sf, "test.txt");
+	sfsadd(&sf, "test.txt");
 	sfsls(sf);
 	sfsdel(sf, "test.txt");
 }
