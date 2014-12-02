1ae7fdf9d0695897cde9d17a9a7f11afce4166a7
Ocsav <monstross.ocsav@gmail.com>
2014-12-03 00:06:49 +0100

    Ajout de la fonction de creation de l'image du systeme de fichier

--- tools/main.c
+++ tools/main.c
@@ -1,12 +1,23 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <fcntl.h>
 #include "structure.h"
 
 extern SimpleFileSystem sfscreate (char *sfsName);
 extern void sfsadd(SimpleFileSystem *sfs , char filename[32]);
 extern void sfsls(SimpleFileSystem sfs);
 extern void sfsdel(SimpleFileSystem *sfs, char filename[32]);
+
+void sfsimg (SimpleFileSystem sfs){
+	int file = open("fs.img", O_WRONLY|O_CREAT, S_IRWXU|S_IRWXG|S_IRWXO);
+	printf("%d\n", sfs.sb.nbSecteurs);
+	write(file, &sfs.sb, 1024);
+	write(file, &sfs.bitmap, 1024);
+	write(file, &sfs.fe, 1024*16);
+	write(file, &sfs.fileContent, 1024);
+	close(file);
+}
 
 int main(int argc, char **argv){
 	SimpleFileSystem sf = sfscreate("sfs");
@@ -15,4 +26,5 @@ int main(int argc, char **argv){
 	sfsls(sf);
 	sfsdel(&sf, "test.txt");
 	sfsls(sf);
+	sfsimg(sf);
 }
