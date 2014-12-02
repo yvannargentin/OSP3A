f3e42d51f668b15af525f39e92aaf360c7f8dc4b
chwebteam1 <infochwebteam.ch>
2014-12-02 19:56:26 +0100

    creation des interuptions et des entetes de fonction

--- kernel/syscall_handler.c
+++ kernel/syscall_handler.c
@@ -2,6 +2,11 @@ extern void print_string(char *str);
 extern void read_string(char *buf);
 extern void read_sector(int sector, uchar *buf);
 extern void write_sector(int sector, uchar *buf);
+extern void iterator();
+extern int get_stat(char *filename, struct stat_st *stat);
+extern int read_file(char *filename, unsigned char *buf);
+extern int remove_file(char *filename);
+
 
 typedef unsigned int uint;
 
@@ -10,6 +15,10 @@ void syscall_handler(uint syscall_nb, uint arg1, uint arg2, uint arg3, uint arg4
 	// 2 : void read_string(char *buf);
 	// 3 : void read_sector(int sector, uchar *buf);
 	// 4 : void write_sector(int sector, uchar *buf);
+	// 5 : void iterator();
+	// 6 : int get_stat(char *filename, struct stat_st *stat);
+	// 7 : int read_file(char *filename, unsigned char *buf);
+	// 8 : int remove_file(char *filename);
 
 	switch(syscall_nb) {
 
@@ -33,6 +42,29 @@ void syscall_handler(uint syscall_nb, uint arg1, uint arg2, uint arg3, uint arg4
 			//arg1 = sector number
 			//arg2 = buffer to hold read string
 			write_sector(arg1, arg2);
+		break;
+
+		case 5 : 
+			iterator();
+		break;
+
+		
+		case 6 : 
+			// arg1 = char *filename
+			// arg2 = struct stat_st *stat
+			get_stat(arg1,arg2);
+		break;
+
+
+		case 7 : 
+			// arg1 = char *filename
+			// arg2 = unsigned char *buf
+			read_file(arg1,arg2);
+		break;
+
+		case 8 : 
+			// arg1 = char *filename
+			remove_file(arg1)
 		break;
 		
 		default : print_string("invalid use of interruption 0x80");
--- tools/sfsls.c
+++ tools/sfsls.c
@@ -12,10 +12,11 @@ void sfsls(SimpleFileSystem monSFS) {
 		if(nbFiles >= monSFS.sb.tailleFE - 1   || monSFS.fe[nbFiles].size == 0)
 			continuer = false;
 		if(continuer){
-			printf(" - %s - %d -\r\n",monSFS.fe[nbFiles].name,monSFS.fe[nbFiles].size);
-			nbFiles++;
+			if(monSFS.fe[nbFiles].name[0] != 0){
+				printf(" - %s - %d -\r\n",monSFS.fe[nbFiles].name,monSFS.fe[nbFiles].size);
+			}
+				nbFiles++;
 		}
 	}
-	printf("pour un total de %d fichiers...\r\n",nbFiles);
 
 }
