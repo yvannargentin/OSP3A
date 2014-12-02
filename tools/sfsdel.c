67a52f41fad9f7b4cd9498c220c8cff9e3f553d7
pannufle <hottinger.jeremy@gmail.com>
2014-12-02 12:28:48 +0100

    Changement sfsdel

--- tools/sfsdel.c
+++ tools/sfsdel.c
@@ -3,27 +3,28 @@
 #include "structure.h"
 
 
-void sfsdel(SimpleFileSystem sfs, char file[32]) {
-	//char fileToDelete[32] = file;
-	
+void sfsdel(SimpleFileSystem *sfs, char filename[32]) {
+	printf("Welcome in sfsdel");
 	int fileEntryIndex = 0;
 		
 	// Parcours des file entries	
-	while(sfs.fe[fileEntryIndex].name != file) {
+	while(sfs->fe[fileEntryIndex].name != filename) {
 		fileEntryIndex++;
 	}
 
-	// 1er bit du name a 0
-	sfs.fe[fileEntryIndex].name[0] = 0;
+	printf("fileEntryIndex : %d", fileEntryIndex);
+
+	// 1er byte du name a 0
+	sfs->fe[fileEntryIndex].name[0] = 0;
 
 
-	FileEntries entry = sfs.fe[fileEntryIndex];
+	// FileEntries *entry = sfs->fe[fileEntryIndex];
 	// Une fois qu'on a la bonne file entry, boucle sur les Index de blocs et modif du bits correspondant dans bitmap
 	int indexInd = 0;
-	while(entry.tabIndexes[indexInd] != 0) {
-		int indexBitmap = entry.tabIndexes[indexInd]/8; //Division entière
-		int decalage = entry.tabIndexes[indexInd]%8-1; //-1 parce qu'on decale à partir de la droite
-		sfs.bitmap[indexBitmap] &= ~(1<<decalage);
+	while(sfs->fe[fileEntryIndex].tabIndexes[indexInd] != 0) {
+		int indexBitmap = sfs->fe[fileEntryIndex].tabIndexes[indexInd]/8; //Division entière
+		int decalage = sfs->fe[fileEntryIndex].tabIndexes[indexInd]%8-1; //-1 parce qu'on decale à partir de la droite
+		sfs->bitmap[indexBitmap] &= ~(1<<decalage);
 		indexInd++;
 	}
 }
