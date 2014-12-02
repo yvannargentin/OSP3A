3c9418a09b22bf622986fb2123961d33cbcffd4f
Ana Sofia Domingos <ana.domingos@hotmail.fr>
2014-12-02 18:46:48 +0100

    new main

--- tools/main.c
+++ tools/main.c
@@ -6,12 +6,12 @@
 extern SimpleFileSystem sfscreate (char *sfsName);
 extern void sfsadd(SimpleFileSystem *sfs , char filename[32]);
 extern void sfsls(SimpleFileSystem sfs);
-extern void sfsdel(SimpleFileSystem sfs, char filename[32]);
+extern void sfsdel(SimpleFileSystem *sfs, char filename[32]);
 
 int main(int argc, char **argv){
 	SimpleFileSystem sf = sfscreate("sfs");
-	sfsadd(&sf, "test.txt");
-	sfsadd(&sf, "fichier.txt");
+	sfsadd(&sf, "test.txt\0");
+	sfsadd(&sf, "fichier.txt\0");
 	sfsls(sf);
-	sfsdel(sf, "test.txt");
+	sfsdel(&sf, "test.txt");
 }
