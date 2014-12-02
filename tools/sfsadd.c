29552ad987c566b3b0c3180c40f17ca272200fc7
Ana Sofia Domingos <ana.domingos@hotmail.fr>
2014-12-02 20:19:49 +0100

    sfsadd corriger pour que sfsdel marche

--- tools/sfsadd.c
+++ tools/sfsadd.c
@@ -16,44 +16,50 @@ void sfsadd(SimpleFileSystem *sfs , char filename[LENGTH]){
 	fp = fopen(filename, "r");
 	char contenu[TAILLE];
 	bool fileInit = false;
+	int nbindex = 0; // nombre de tabIndexes qu'on aura dans le file entry
+	
+	// recupere l'id du fichier pour l'ajouter 
+	int idFiles = 0;
+	while (sfs->fe[idFiles].size != 0)
+		idFiles++;
 	
 	// lecture du bitmap
 	for (i=0; i< BLOCK; i++){	
 		for (j=0; j< BIT; j++){
 			if ((sfs->bitmap[i]&(1<<j)) == 0){ // si bit block libre
-				
 				if (fp == NULL){
 					printf("impossible d'ouvrir le fichier\n");
 					return; 
 				}else{
-					int index = (i*BIT)+j;
-					sfs->bitmap[index] = 1; // bit block utilisé
-					
+					int index = (1<<j);
+					sfs->bitmap[i] |= index; // bit block utilisé
+
 					// initalisation du file entry avec le nom et la taille	
 					if (fileInit == false){
 						// recupere le nom du fichier
 						while (filename[h] != '\0'){
-							sfs->fe[index].name[h] = filename[h];
+							sfs->fe[idFiles].name[h] = filename[h];
 							h++;
 						}
 						// recuperer taille fichier
-						sfs->fe[index].size = FileSize(filename);
+						sfs->fe[idFiles].size = FileSize(filename);
 						fileInit = true;
+						
 					}
-
 					// recupere l'index dans lequel est ranger le contenu
-					sfs->fe[index].tabIndexes[index] = index;
-					
+					sfs->fe[idFiles].tabIndexes[nbindex] = index;	
+
 					fgets(contenu, TAILLE, fp); // recupere contenu fichier	
 
 					// ajoute le contenu du fichier par block dans le fileContent correspondant
 					for (r=0; r< TAILLE; r++){
-						int index = (r*BIT)+j;
+						//int id = (r*BIT)+j;
 						sfs->fileContent[index][r] = contenu[r];				
 					}
 				
 					// tant qu'on arrive pas à la fin du fichier
 					if(fgetc(fp) != EOF){
+						nbindex++;
 						// vide le tableau contenant le contenu
 						for (r=0; r< TAILLE; r++)
 							contenu[r] = '\0';
@@ -70,7 +76,7 @@ void sfsadd(SimpleFileSystem *sfs , char filename[LENGTH]){
 	}
 }
 
-
+// Calcule la taille du fichier
 int FileSize(char filename[LENGTH]){
 	// ouvre le fichier pour le parcourir
 	FILE *fp;
@@ -87,3 +93,4 @@ int FileSize(char filename[LENGTH]){
 
 	return taille;
 }
+
