a78e7b2c38438b2814aa64f13b75516b8d1a3d0b
chwebteam1 <infochwebteam.ch>
2014-12-05 19:11:43 +0100

    rajout de defines par rapport au sfs

--- kernel/nomenclature.h
+++ kernel/nomenclature.h
@@ -6,4 +6,9 @@
 #define get_stat 6
 #define read_file 7
 #define remove_file 8
+#define debutSuperBlock 20
+#define debutBitmap 22
+#define debutFe 24
+#define debutFileContent (debutFe + 32)
+#define tailleFileEntry 256
 
--- kernel/sfs.c
+++ kernel/sfs.c
@@ -2,10 +2,15 @@ int interrupt(int number, int ax, int bx, int cx, int dx, int di);
 #include "nomenclature.h"
 
 typedef unsigned char uchar;
+
+// le sfs est écris au secteur 20 le Superblock prend 2 secteur
+// le bitmap prends 2 secteurs donc on commence le FileEntrie à 24
+
+#define tailleFileEntry 256
+int decalage = 0;
 void iterator() {
-	uchar buf[512];
-	// le sfs est écris au secteur 20
-	interrupt(0x80,read_sect,24, buf,0,0);
+	uchar buf[tailleFileEntry]; 
+	interrupt(0x80,read_sect,StartFileEntries + decalage, buf,0,0);
 	
 }
 
@@ -17,3 +22,4 @@ int read_file(char *filename, unsigned char *buf) {
 
 int remove_file(char *filename) {
 }
+
