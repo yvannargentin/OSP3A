c5960e94693a7c77c28d1351615619d4092c164c
AnaSofiaDomingos <ana.domingos@hotmail.fr>
2014-12-09 15:48:08 +0100

    ajout de read_file

---
+++ kernel/read_file.c
@@ -0,0 +1,48 @@
+#include "nomenclature.h"
+
+int read_file(char *filename, unsigned char *buf){
+	int nb_sector_fe;
+	int offset;
+	unsigned char sect[512];
+
+	// recuperer le secteur et l'offset du fe correponsdant
+	do {
+		if (iteratorInterne(nb_sector_fe, offset) == null)
+			return -1;
+		else{
+			iteratorInterne(nb_sector_fe, offset);
+			interrupt(0x80,read_sect,nb_sector_fe, sect,0,0); // lit un secteur qu'on vient d'écrire 
+		}	
+	} while (strcomp(sect[offset]), filename);
+
+	int debutTabIndexes = 34;
+	buf = "";
+	// parcours la liste des tabIndexes tant que != 0
+	while (sect[debutIndexes] != 0){
+		// lit contenu du filecontent a l'index correspondant
+		int nb_sector_fc = debutFileContent + sect[debutIndexes];	
+		unsigned char contenu1[512];
+		unsigned char contenu2[512];
+		
+		interrupt(0x80,read_sect,nb_sector_fc, contenu1,0,0); 
+		interrupt(0x80,read_sect,nb_sector_fc+1, contenu2,0,0);  
+		
+	
+		// ajoute au buffer le contenu du filecontent
+		buf += contenu1;
+		buf += contenu2;
+		nb_sector_fc += 2; 
+		debutIndexes++;
+	}
+	
+	return 0;	
+}
+
+
+int strcomp(const char *s1, const char *s2)
+{
+    for ( ; *s1 == *s2; s1++, s2++)
+	if (*s1 == '\0')
+	    return 0;
+    return ((*(unsigned char *)s1 < *(unsigned char *)s2) ? -1 : +1);
+}
