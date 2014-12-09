d8e220421e3233d7feb21a8dcf9af2bbce7acbe2
pannufle <hottinger.jeremy@gmail.com>
2014-12-09 21:37:17 +0100

    Update remove_file

--- kernel/remove_file.c
+++ kernel/remove_file.c
@@ -1,5 +1,6 @@
 extern int strcomp(const char *s1, const char *s2);
 extern int interrupt(int number, int ax, int bx, int cx, int dx, int di);
+extern int modulo(int a, int b);
 #include "nomenclature.h"
 typedef unsigned char uchar;
 
@@ -13,44 +14,59 @@ int remove_file(char *filename) {
 	unsigned char map[BlockSize];
 	int debutIndexes = 34;
 	int indexFile;
+	int indexBitmap;
+	int decalage;
+	int indexF;
 
 	do {
 		
-		interrupt(0x80,read_sect,noSector, buf,0,0); // lit un secteur qu'on vient d'écrire 
-		
-		if(modulo(compteur,2) == 1)
+		interrupt(0x80,read_sect,noSector, buf,0,0);
+		// interrupt(0x80,print_str,buf,0,0,0); 
+
+		if((compteur%2) == 1)
 			noSector++;
+		
 		if(offset == 0)
 			offset = BlockSize/2;
 		else
 			offset = 0;
 
-		compteur++;
-
+		compteur++; 
 		
-	} while (strcomp(buf[offset],filename));
+	} while (strcomp(&buf[offset],filename) != 0 || compteur > 64);
+
+	interrupt(0x80,print_str,buf,0,0,0); 
 
 	// byte 0 of name = 0
 	buf[offset] = 0;
+	interrupt(0x80,print_str,"First name byte changed to 0",0,0,0); 
 
 	// Load bitmap
 	interrupt(0x80, read_sect, BtmStart, map, 0, 0);
+	interrupt(0x80,print_str,"Loading bitmap from sect 22",0,0,0); 
 
 
 	indexFile = offset+34;
+	indexF = &buf[indexFile] - 0;
 
 	// Iterate on fileIndexes
-	while (buf[indexFile] != 0) {
-		int indexBitmap = buf[indexFile]/8;
-		int decalage = modulo(buf[indexFile],8-1);
+	while (indexF != 0) {
+		indexF = &buf[indexFile] - 0;
+		interrupt(0x80,print_str,"Iterating...",0,0,0); 
+		interrupt(0x80,print_str,&buf[indexFile],0,0,0); 
+
+		indexBitmap = buf[indexFile]/8;
+		decalage = indexF%8-1;
 
 		map[indexBitmap] &= ~(1<<decalage);
 		indexFile += 2;
+
+		
 	}
 
 	// Saving bitmap to image
-	interrupt(0x80, write_sect, BtmStart, map, 0, 0);
+	/*interrupt(0x80, write_sect, BtmStart, map, 0, 0);
 	// Saving file entry sector
-	interrupt(0x80, write_sect,noSector, buf, 0, 0);
+	interrupt(0x80, write_sect,noSector, buf, 0, 0); */
 }
 
