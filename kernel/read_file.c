4e692bf18138ab3c111a9b5210e7064287c070d9
AnaSofiaDomingos <ana.domingos@hotmail.fr>
2014-12-09 23:57:04 +0100

    read_file marche presque manque reussir a recuperer les tabIndexes

--- kernel/read_file.c
+++ kernel/read_file.c
@@ -1,50 +1,66 @@
 #include "nomenclature.h"
 extern int strcomp(const char *s1, const char *s2);
-extern int interrupt(int number, int ax, int bx, int cx, int dx, int di);
 
 int read_file(char *filename, unsigned char *buf){
 	int nb_sector_fe = FeStart;
 	int offset = 0;
 	unsigned char sect[BlockSize];
-	int compteur = 0;
-	int debutIndexes = 34;
-	
+	int cpt = 0;
+	int index;
+	int indexes = TabIndexesStart;
+	int nb_sector_fc = 0;
+	unsigned char contenu[BlockSize];
 
-	// recuperer le secteur et l'offset du fe correponsdant
+	//get the right sector number and the buffer of this sector
+	//a sector as 2 FileEntries
 	do {
-		
-		interrupt(0x80,read_sect,nb_sector_fe, sect,0,0); // lit un secteur qu'on vient d'écrire 
-		
-		if(modulo(compteur,2) == 1)
+		interrupt(0x80,read_sect,nb_sector_fe, sect,0,0); 
+
+		if(cpt%2 == 1)
 			nb_sector_fe++;
+
+		// offset is to know which FileEntry we are going to use in this sector
 		if(offset == 0)
-			offset = BlockSize/2;
+			offset = BlockSize/2; 
 		else
 			offset = 0;
 
-		compteur++;
+		// exit if the file is note found
+		if (cpt > MaxFe)
+			return -1;
 
-		
-	} while (strcomp(sect[offset],filename));
+		cpt++;
+	} while (strcomp(&sect[offset],filename) != 0);
 
-	buf = "";
-	// parcours la liste des tabIndexes tant que != 0
-	while (sect[debutIndexes] != 0){
-		// lit contenu du filecontent a l'index correspondant
-		int nb_sector_fc = FCStart + sect[debutIndexes];	
-		unsigned char contenu1[BlockSize];
-		unsigned char contenu2[BlockSize];
+	indexes += offset; // get where the tabIndexes starts
+	index = 1 ; //&sect[indexes];
+
+	// iterate on the tabIndexes used
+	//while (index != 0){
+
+		nb_sector_fc = FCStart + index;	
 		
-		interrupt(0x80,read_sect,nb_sector_fc, contenu1,0,0); 
-		interrupt(0x80,read_sect,nb_sector_fc+1, contenu2,0,0);  
+		//print_string("iterating on the tabindexes");
 		
-	
-		// ajoute au buffer le contenu du filecontent
-		//buf += contenu1;
-		//buf += contenu2;
-		nb_sector_fc += 2; 
-		debutIndexes++;
-	}
+		// read the content of fileContent 
+		interrupt(0x80,read_sect,nb_sector_fc, contenu,0,0); 
+		// content separted in 2 sectors
+		if (length(contenu) == BlockSize -1)
+			interrupt(0x80,read_sect,nb_sector_fc+1, contenu,0,0);
+		interrupt(0x80,print_str, contenu, 0, 0,0);
+		nb_sector_fc += 2; // jump to the next fileContent
+		index++;	//jump to the next tabIndexes
+	//}
 	
 	return 0;	
 }
+
+// get the length of a table of char
+int length(char* tab){
+	int i = 0;
+	while(tab[i] !='\0'){		
+		i += 1;
+	}
+	return i;
+}
+
