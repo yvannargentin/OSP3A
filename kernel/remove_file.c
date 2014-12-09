31862af8bb886250472ab9f7aeb3ca9e41817f26
chwebteam1 <infochwebteam.ch>
2014-12-09 16:14:00 +0100

    fonction modulo assembleur (pas sur du tout que ca marche mais on peut l'appeler) int modulo(valeur, diviseur)

--- kernel/remove_file.c
+++ kernel/remove_file.c
@@ -1,5 +1,6 @@
 extern int strcomp(const char *s1, const char *s2);
 extern int interrupt(int number, int ax, int bx, int cx, int dx, int di);
+#include "nomenclature.h"
 typedef unsigned char uchar;
 
 int remove_file(char *filename) {
--- kernel/sfs.c
+++ kernel/sfs.c
@@ -5,7 +5,7 @@ typedef unsigned char uchar;
 
 
 int divfloor(int val,int divisor);
-int modulo(int val, int divisor); // temporary
+int strcomp(const char *s1, const char *s2);
 
 int iterator(int counter, char *buf) {
 	// divide by 2
@@ -18,21 +18,22 @@ int get_stat(char *filename, struct stat_st *stat) {
 }
 
 int divfloor(int val,int divisor){
-	int result = 0;/*
+	int result = 0;
 	while(modulo(val, divisor) != 0)
 		val -= 1; 
-	*/
+	
 	result = val / divisor;
 	
 	return result;
 }
 
 
-int strcomp(const char *s1, const char *s2) {
-    for ( ; *s1 == *s2; s1++, s2++)
-	if (*s1 == '\0')
-	    return 0;
-    return ((*(unsigned char *)s1 < *(unsigned char *)s2) ? -1 : +1);
+int strcomp(char *s1, char *s2) {
+	for ( ; *s1 == *s2; s1++, s2++){
+		if (*s1 == '\0')
+		    return 0;
+	}
+	return ((*(unsigned char *)s1 < *(unsigned char *)s2) ? -1 : +1);
 }
 
 
