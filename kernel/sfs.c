33685bf17a2203d15631e8e663700909d6838ead
pannufle <hottinger.jeremy@gmail.com>
2014-12-10 10:44:40 +0100

    Ajout des fonctions remove_file et get_stat

--- kernel/sfs.c
+++ kernel/sfs.c
@@ -34,8 +34,101 @@ int iterator(int isOk, char *buf) {
 }
 
 int get_stat(char *filename, struct stat_st *stat) {
+	
+	uchar buf[BlockSize/2];
+	int offset_size = 32;	
+
+	do {
+		iterator(0, buf) {
+	} while (strcomp(buf, filename) != 0);
+
+	int tmp = buf[offset_size++];
+	int tmp2 = buf[offset_size];
+	int size = tmp+(tmp2<<8);
+	
+	stat->filename = filename;
+	stat->size = size;
+	
+	int length_str = lengthInt(size);
+	char str[length_str]; //espace prevu pour le \0
+	intTostr(str, test, length_str);
+
+	interrupt(0x80,print_str,str,0,0,0);
 }
 
+/* remove_file.c
+ * Removes a file which name's given in parameter
+ */
+int remove_file(char *filename) {	
+	// Iterate on fileEntries
+	uchar buf[BlockSize];
+	int noSector = FeStart;	// Sector number containing fe (24 for the first one)
+	int offset = 0;		// offset of fe in sector (0 or 256)
+	int counter = 0;
+	unsigned char map[BlockSize];
+	int debutIndexes = 34;
+	int indexFile;
+	int indexBitmap;
+	int decalage;
+	int indexF;
+	int tmp;
+	int tmp2;
+
+	// Find 
+	do {
+		interrupt(0x80,read_sect,noSector, buf,0,0);
+		// Inc counter each 2 fe (one sector contains 2 fe)
+		if((counter%2) == 1)
+			noSector++;
+		// Offset is 0 or 256 (BlockSize/2). fe is either at the begining of the sector or just at the middle (2fe for each sectors)
+		if(offset == 0)
+			offset = FESize;
+		else
+			offset = 0;
+		counter++; 
+		
+	} while (strcomp(&buf[offset],filename) != 0 || counter > 64);
+	
+	if(counter > 64)
+		return -1;	// File not found
+
+	// byte 0 of name = 0
+	buf[offset] = 0;
+	interrupt(0x80,print_str,"First name byte changed to 0",0,0,0); 
+
+	// Load bitmap
+	interrupt(0x80, read_sect, BtmStart, map, 0, 0);
+	interrupt(0x80,print_str,"Loading bitmap from sect 22",0,0,0); 
+
+	// Get the offset of the first file index
+	indexFile = offset+debutIndexes;
+
+	// Iterate on fileIndexes
+	do {
+		// Retreives the int value of file index
+		tmp = buf[indexFile++];
+		tmp2 = buf[indexFile++];
+		indexF = tmp+(tmp2<<8);
+
+		interrupt(0x80,print_str,"Iterating...",0,0,0); 
+
+		// Find the bit to change in the bitmap
+		indexBitmap = indexF/8;
+		decalage = indexF%8-1;
+		// Put the bit to 0
+		interrupt(0x80,print_str,&map[indexBitmap],0,0,0); 
+		map[indexBitmap] &= ~(1<<decalage);
+	} while(indexF != 0);
+
+	// Saving bitmap to image
+	interrupt(0x80, write_sect, BtmStart, map, 0, 0);
+	interrupt(0x80,print_str,"Bitmap saved",0,0,0);  
+	// Saving file entry sector
+	interrupt(0x80, write_sect, noSector-1, buf, 0, 0); 
+	interrupt(0x80,print_str,"File entry saved",0,0,0);
+
+	return 0;
+}
 
 int strcomp(char *s1, char *s2) {
 	for ( ; *s1 == *s2; s1++, s2++){
