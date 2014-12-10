12159af9517bd2955c79d8342d8d2174444502a9
pannufle <hottinger.jeremy@gmail.com>
2014-12-10 09:54:39 +0100

    add int to str

--- kernel/intTostr.c
+++ kernel/intTostr.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 int myPow(int a,int b){
       if(b<0)      
         return (1.0/a)*(myPow(a,b+1)); 
@@ -10,22 +9,40 @@ int myPow(int a,int b){
         return a*myPow(a,b-1);
 }
 
-void intTostr(char* s, int a) {
+void intTostr(char* s, int a, int length_str) {
 	int taille_chaine = 1; 	
-		
-	while(a/myPow(10, taille_chaine) != 0){	
-		taille_chaine++;
-	}
 
-	char result[taille_chaine+1];
+	char result[length_str];
 	
 	int i, puissance10, unite;
-	for(i=0; i<taille_chaine; i++){
-		puissance10 = a/myPow(10, taille_chaine-i-1);
+	int nb_numbers = length_str-1;
+	for(i=0; i<nb_numbers; i++){
+		puissance10 = a/myPow(10, nb_numbers-i-1);
 		unite = puissance10 % 10; //Recuperation de l'unite
 		result[i] = unite + 48; //48 = 0 en ascii;
 	}
-	result[taille_chaine] = '\0';
+	result[length_str-1] = '\0';
 	
-	s = result;//Problème de pointeurs HELP !!!!
+	for(i=0; i<length_str; i++){
+		s[i] = result[i];
+	}
 }
+
+int lengthInt(int a){
+	int length = 1;
+	while(a/myPow(10, length) != 0){	
+		length++;
+	}
+	length++;
+	return length;
+}
+
+/*Exemple
+void main(void){
+	int test = 1234;
+	
+	int length_str = lengthInt(test);
+	char str[length_str]; //+1 => espace en plus pour le \0
+	intTostr(str, test, length_str);
+	printf("%s\n", str);
+}*/
